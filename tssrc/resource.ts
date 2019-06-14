c923f0fbe051be563896649df2e203cc95d67406
demon-monk <antdude@126.com>
2019-06-14 19:41:22 +0800

    feat: add resources for rapid project

--- tssrc/resource.ts
+++ tssrc/resource.ts
@@ -24,6 +24,20 @@
 
 export const res: {[key: string]: string} = {
     HelloWorld_png : "res/HelloWorld.png",
+    Sprite_Image:"res/sprite_image.png",
+    Sprite_Sheet:"res/sprite_sheet.png",
+    Sprite_Sheet1:"res/spritesheetplist-0.png",
+    Sprite_Sheet1_P:"res/spritesheetplist.plist",
+    Custom_TTF:"res/Abduction.ttf",
+    BM_Font:"res/bitmapFontTest.fnt",
+    BM_Font_Png:"res/bitmapFontTest.png",
+    MenuItemImage_Normal:"res/menuitem_normal.png",
+    MenuItemImage_Selected:"res/menuitem_selected.png",
+    ButtonImage:"res/button.png",
+    ButtonImage_Selected:"res/button_selected.png",
+    CheckBox_Normal:"res/checkbox_normal.png",
+    CheckBox_Selected:"res/checkbox_selected.png",
+    Tail:"res/streak.png"
 };
 
 export const g_resources = Object.keys(res).map(key => res[key])
